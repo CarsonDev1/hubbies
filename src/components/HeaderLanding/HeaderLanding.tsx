60647e1730d962195b8f2fa229c783ecdba3b0aa
CarsonDev1 <buitritinht@gmail.com>
2024-10-04 08:50:36 +0700

    fix header

--- src/components/HeaderLanding/HeaderLanding.tsx
+++ src/components/HeaderLanding/HeaderLanding.tsx
@@ -3,9 +3,9 @@ import Logo from '../../assets/images/logo.png';
 
 export default function HeaderLanding() {
 	return (
-		<header className='bg-[#FFFBF2] py-4 px-6 container mx-auto flex justify-between items-center border-b border-[#8B4513]'>
+		<header className='bg-[#FFFBF2] py-4 px-4 sm:px-6 container mx-auto flex justify-between items-center border-b border-[#8B4513]'>
 			<img src={Logo} alt='Logo' />
-			<div className='space-x-4'>
+			<div className='flex flex-col md:flex-row gap-2 items-center'>
 				<Button className='px-12 text-white rounded-full bg-button-color hover:bg-button-color/90'>
 					Login
 				</Button>
