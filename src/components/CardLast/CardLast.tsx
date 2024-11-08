ed2890e14df3be3322c643820ab7402e4928a8ef
CarsonDev1 <buitritinht@gmail.com>
2024-11-08 14:24:08 +0700

    upload code

--- src/components/CardLast/CardLast.tsx
+++ src/components/CardLast/CardLast.tsx
@@ -2,7 +2,7 @@ import CardLastImage from '../../assets/images/card-last.png';
 
 const CardLast = () => {
 	return (
-		<div className='grid grid-cols-1 sm:grid-cols-2 gap-4 mb-6 md:grid-cols-3'>
+		<div className='grid grid-cols-2 sm:grid-cols-3 gap-4 mb-6 md:grid-cols-4'>
 			<div className='bg-[#98971a] text-white p-4 rounded-lg relative w-full overflow-hidden'>
 				<img
 					src={CardLastImage}
@@ -12,7 +12,7 @@ const CardLast = () => {
 					className='absolute top-0 left-0 z-0 object-cover w-full h-full rounded-md'
 				/>
 				<div className='relative z-10'>
-					<h2 className='text-4xl font-bold'>7</h2>
+					<h2 className='text-2xl font-bold'>7</h2>
 					<p>Event created</p>
 				</div>
 			</div>
@@ -25,8 +25,10 @@ const CardLast = () => {
 					className='absolute top-0 left-0 z-0 object-cover w-full h-full rounded-md'
 				/>
 				<div className='relative z-10'>
-					<h2 className='text-4xl font-bold'>17.800.000 VND</h2>
 					<p>Revenue</p>
+					<h2 className='text-sm md:text-xl font-bold'>1.246.000 VND</h2>
+					<p>Profit</p>
+					<h2 className='text-sm md:text-xl font-bold'>17.800.000 VND</h2>
 				</div>
 			</div>
 			<div className='bg-[#98971a] text-white p-4 rounded-lg relative w-full overflow-hidden'>
@@ -38,8 +40,21 @@ const CardLast = () => {
 					className='absolute top-0 left-0 z-0 object-cover w-full h-full rounded-md'
 				/>
 				<div className='relative z-10'>
-					<h2 className='text-4xl font-bold'>21</h2>
+					<h2 className='text-2xl font-bold'>21</h2>
 					<p>Join</p>
+				</div>
+			</div>
+			<div className='bg-[#98971a] text-white p-4 rounded-lg relative w-full overflow-hidden'>
+				<img
+					src={CardLastImage}
+					alt='cart-last'
+					width={800}
+					height={500}
+					className='absolute top-0 left-0 z-0 object-cover w-full h-full rounded-md'
+				/>
+				<div className='relative z-10'>
+					<h2 className='text-2xl font-bold'>104</h2>
+					<p>Order</p>
 				</div>
 			</div>
 		</div>
